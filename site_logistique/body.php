3ef7446e7edd91289dffb55856130c417380d876
khalil-rais <104235580+khalil-rais@users.noreply.github.com>
2022-04-23 13:58:04 +0200

    Add files via upload

---
+++ site_logistique/body.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style_body.css">
+	</head>
+	<body>
+		<p><span class="titre">' . t('Type marchandise') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Dimensions et charge') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Date début') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Date arrivée ') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Lieu départ') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Lieu arrivé') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Véhicule souhaité') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Remarque') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Nom et Prénom') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Ville, région et Pays') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Téléphone') . '</span><span class="valeur"></span></p>
+		<p><span class="titre">' . t('Email') . '</span><span class="valeur"></span></p>
+	</body>
+</html>
